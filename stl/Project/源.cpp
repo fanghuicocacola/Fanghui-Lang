6b8129fcb2db3097bb19300c4dd82b1faace9e24
fanghuicocacola <56107518+fanghuicocacola@users.noreply.github.com>
2020-02-05 21:12:04 +0800

    啦啦啦

--- stl/Project/源.cpp
+++ stl/Project/源.cpp
@@ -13,23 +13,26 @@ int main(int argc, char* argv[]) {
 	SDL_DestroyWindow(window);//销毁窗口
 	SDL_Quit();//退出
 	return 0;*/
-	SDL_Init(SDL_INIT_VIDEO);
-	SDL_Window* window = SDL_CreateWindow("SDL Tutoriall:)",
-		SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,//用“100，100”就是自己确定窗口位置
-		800, 600,
-		SDL_WINDOW_SHOWN);
-	SDL_Rect rect;
-	rect.x = 100;
-	rect.y = 100;
+
+		SDL_Init(SDL_INIT_VIDEO);
+		// windows title , pos_x, pos_y, windows_size,windows mod.
+		SDL_Window* window = SDL_CreateWindow("Lana Del Ray", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+			782, 900,
+			SDL_WINDOW_SHOWN);
+		SDL_Surface* surface = SDL_GetWindowSurface(window);
+
+		SDL_Rect rect;
+		rect.x = 0;
+		rect.y = 0;
+			SDL_Surface* image = SDL_LoadBMP("sample.bmp");
+
+			SDL_BlitSurface(image, NULL, surface, &rect);
+
+		SDL_UpdateWindowSurface(window);
+		SDL_Delay(30000);
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 0;
+	}
+
 	
-	SDL_Surface* surface = SDL_GetWindowSurface(window);
-	SDL_Surface* image = SDL_LoadBMP("sample.bmp");
-	SDL_BlitSurface(image, NULL, surface, &rect);
-	SDL_UpdateWindowSurface(window);
-	SDL_Delay(5000);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
-	printf_s("hello world");
-	return 0;
-	
-}
