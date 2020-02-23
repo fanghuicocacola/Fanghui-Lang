2fc7f544560791bae48fd2657c1b30a4e67d2862
fanghuicocacola <56107518+fanghuicocacola@users.noreply.github.com>
2020-02-23 18:49:46 +0800

    ok

---
+++ 洛谷/Project1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+﻿#include<stdio.h>
+
+int main() {
+	int a, b,c,d;
+	scanf_s("%d %d %d %d", &a, &b, &c, &d);
+	int x, y;
+	x = c - a;
+	y = d - b;
+	if (y >= 0) {
+		printf_s("%d %d", x, y);
+	}
+	else {
+		x = x - 1;
+		y = y+60;
+		printf_s("%d %d",x , y);
+	}
+	return 0;
+}
