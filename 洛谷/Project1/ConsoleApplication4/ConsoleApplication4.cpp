0259a0f6ef3562113fe2381088886af9cfebfe19
fanghuicocacola <56107518+fanghuicocacola@users.noreply.github.com>
2020-02-28 22:06:44 +0800

    打卡

---
+++ 洛谷/Project1/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+int main()
+{
+    int n, x;
+    int i,p=0;
+    scanf_s("%d %1d", &n, &x);
+    for (i = 0; i <= n; i++) {
+        int a;
+        a = i;
+        while (a > 0) {
+            int y;
+            y = a % 10;
+            a = a / 10;
+            if (y == x) {
+                p++;
+            }
+
+        }
+    }
+    printf("%d", p);
+    return 0;
+}
