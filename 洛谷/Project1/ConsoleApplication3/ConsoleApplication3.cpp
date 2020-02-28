0f006a2324db43c80360e850f1c16dd30293b127
fanghuicocacola <56107518+fanghuicocacola@users.noreply.github.com>
2020-02-28 15:22:19 +0800

    打卡

---
+++ 洛谷/Project1/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <stdio.h>
+
+int main()
+{
+	int all=0, cast, self=0;
+	for (int mouths = 1; mouths <= 12; mouths++) {
+		scanf_s("%d", &cast);
+		self = self + 300 - cast;
+		if (self < 0) {
+			printf("-%d", mouths);
+			return 0;
+		}
+		if (self>=100) {
+			all = all +(self-self%100)/100;
+			self = self % 100;
+		}
+	}
+	printf("%d",all * 120+self);
+	return 0;
+}
+
+
