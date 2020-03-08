b330a3271bca05512144778c8b491d61e6adb963
fanghuicocacola <56107518+fanghuicocacola@users.noreply.github.com>
2020-03-08 13:18:47 +0800

    daily

---
+++ 洛谷/Project1/ConsoleApplication10/ConsoleApplication10.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include<string.h>
+int main()
+{
+    int n,x;
+    char s[55];
+    scanf_s("%d\n", &n);
+    n = n % 26;
+    gets_s(s);
+    s[strlen(s)+1] = '\0';
+    x = strlen(s);
+    for (int i = 0; i < x; i++) {
+        if (s[i] + n >= 128)
+            s[i] += n - 26;
+        else
+            s[i] += n;
+        while(s[i] > 122) {
+            s[i] = s[i] % 122 + 'a' - 1;
+        }
+        
+    }
+    
+    puts(s);
+   
+    return 0;
+}
+
+
