022ac2289fdc7ff63aa0e59f2233eb6715bada42
fanghuicocacola <56107518+fanghuicocacola@users.noreply.github.com>
2020-03-11 16:14:33 +0800

    打卡

---
+++ 洛谷/Project1/ConsoleApplication11/ConsoleApplication11.cpp
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+int a[1001];
+int main() {
+    int n, i ;
+    a[0] = 1;
+    a[1] = 1;
+    scanf_s("%d", &n);
+    for (i = 2; i <= n; i++) {
+        if (i % 2 == 0) 
+            a[i] = a[i - 1] + a[i / 2];
+        else 
+            a[i] = a[i - 1];
+    }
+    printf("%d\n", a[n]);
+}
