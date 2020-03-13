74eab64a16dce8debbef257d0ae4561d990b35fa
辉 房 <fanghuicocacola@outlook.com>
2020-03-13 14:01:10 +0800

    daily

---
+++ python/PythonApplication8/PythonApplication8/PythonApplication8.py
@@ -0,0 +1,10 @@
+a="wula"
+print(len(a))
+print(max(1,2,3,2342,123,4324,35432,1232,1221,13))
+numbers=[1,2,3,4,5,6,7,8,9,'0']
+numbers.pop()
+b=sum(numbers)
+print(b)
+def add(num1,num2):
+    return(num1+num2)
+print(add(1,2))
