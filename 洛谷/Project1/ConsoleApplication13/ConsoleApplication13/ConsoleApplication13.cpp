14808986541e4252558f0904a4257e9c33965910
辉 房 <fanghuicocacola@outlook.com>
2020-03-18 19:32:37 +0800

    daka

---
+++ 洛谷/Project1/ConsoleApplication13/ConsoleApplication13/ConsoleApplication13.cpp
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+#include <string.h>
+bool is_prime(int x)
+{
+	if (x % 2 == 0)
+		return false;
+	for (int k = 2; k * k <= x; k++)
+		if (x % k == 0) 
+			return false;
+	return true;
+}
+int main()
+{
+	int i, num, n, t = 0, temp, Num;
+	scanf_s("%d%d", &Num, &num);
+	for (i = Num; i <= num; i++)
+	{
+		temp = i;
+		n = 0;
+		while (temp > 0)
+		{
+			n = n * 10 + temp % 10;
+			if (n == temp)
+			{
+				if(is_prime(i))
+					printf("%d\n", i);
+				break;
+			}
+			temp /= 10;
+			if (n == temp)
+			{
+				if(is_prime(i))
+					printf("%d\n", i);
+				break;
+			}
+		}
+	}
+}
