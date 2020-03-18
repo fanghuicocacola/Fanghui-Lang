e35b23c41543ce5ea7d7ec6726456bc3fd98ee60
辉 房 <fanghuicocacola@outlook.com>
2020-03-18 13:51:52 +0800

    daka

---
+++ python/Python Learner/3.18.2.py
@@ -0,0 +1,5 @@
+import math
+a, b, c = map(float, input().split())
+p = (a+b+c)/2
+s = math.sqrt(p*(p-a)*(p-b)*(p-c))
+print(p, s)
---
+++ python/Python Learner/3.18.3.py
@@ -0,0 +1,4 @@
+import time
+for i in range(21, -1, -1):
+    print('\r'+'~'*i, end='')
+    time.sleep(0.2)
