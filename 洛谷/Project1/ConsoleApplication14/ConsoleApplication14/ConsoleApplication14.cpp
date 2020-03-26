e048c4e7e95a8252793f82a04dfe4519b39e4fc0
辉 房 <fanghuicocacola@outlook.com>
2020-03-26 20:44:02 +0800

    打卡

---
+++ 洛谷/Project1/ConsoleApplication14/ConsoleApplication14/ConsoleApplication14.cpp
@@ -0,0 +1,51 @@
+﻿#include<stdio.h>
+int main()
+{
+	int a, b, c, temp;
+	scanf_s("%d%d%d", &a, &b, &c);
+	if (a > b)
+	{
+		temp = a;
+		a = b;
+		b = temp;
+	}
+	if (a > c)
+	{
+		temp = a;
+		a = c;
+		c = temp;
+	}
+	if (b > c)
+	{
+		temp = b;
+		b = c;
+		c = temp;
+	}
+	if (a + b > c)
+	{
+		if ((a * a + b * b) == c * c)
+		{
+			printf("Right triangle\n");
+		}
+		else if ((a * a + b * b) > c * c)
+		{
+			printf("Acute triangle\n");
+		}
+		else if ((a * a + b * b) < c * c)
+		{
+			printf("Obtuse triangle\n");
+		}
+		if (a == b || b==c || a==c)
+		{
+			printf("Isosceles triangle\n");
+		}
+		if (a == b && b == c && a == c)
+		{
+			printf("Equilateral triangle\n");
+		}
+	}
+	else
+	{
+		printf("Not triangle\n");
+	}
+}
