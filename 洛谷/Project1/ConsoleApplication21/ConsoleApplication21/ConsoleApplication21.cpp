54d662329231a8dc697f8219abd2963d4b2077db
辉 房 <fanghuicocacola@outlook.com>
2020-04-12 17:17:18 +0800

    daka

---
+++ 洛谷/Project1/ConsoleApplication21/ConsoleApplication21/ConsoleApplication21.cpp
@@ -0,0 +1,17 @@
+﻿#include<stdio.h>
+int a[11];
+int main()
+{
+	int j, m, n;
+	scanf_s("%d%d", &m, &n);
+	for (int i = m; i <= n; i++) {
+		j = i;
+		while (j > 0) {
+			a[j % 10]++;
+			j = j / 10;
+		}
+	}
+	for (int i = 0; i < 10; i++) {
+		printf("%d ", a[i]);
+	}
+}
