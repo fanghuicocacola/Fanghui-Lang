fcebac382f76918fc98843e7dbbe7213370b67ad
辉 房 <fanghuicocacola@outlook.com>
2020-04-27 18:25:14 +0800

    daka

--- python/Python Learner/4.201.py
+++ python/Python Learner/4.201.py
@@ -1,11 +1,20 @@
-str1 = 'boys are good,boys like VS'
+s = 'boys are good,boys like VS'
 # 方法一
-str2 = str1.split()
+s1 = ''
+s2 = s.split()
 n = 0
-for i in str2:
+for i in s:
+    if i.isalnum() or i == ' ':
+        s1 = s1 + i
+    else:
+        i = ' '
+        s1 = s1 + i
+print(s1)
+s2 = s1.split()
+for i in s2:
     if 'boys' in i:
         n += 1
 print(n)
 # 方法二
-n = str1.count("boys", 0)   # 统计字符串中 \\ 出现的次数
+n = s1.count("boys", 0)   # 统计字符串中 \\ 出现的次数
 print(n)
--- python/Python Learner/study11.py
+++ python/Python Learner/study11.py
@@ -1,7 +1,17 @@
-str1 = 'boys are good,boys like VS'
-str2 = str1.split()
+s = 'boys are good,boys like VS'
+s1 = ''
 n = 0
-for i in str2:
-    if 'boys' in i:
-        n += 1
-print(n)
+for i in s:
+    if i.isalnum() or i == ' ':
+        s1 = s1 + i
+    else:
+        i = ' '
+        s1 = s1 + i
+print(s1)
+s2 = s1.split()
+s3 = []
+for j in s2:
+    if s2.count(j) >= 2:
+        if j not in s3:
+            s3.append(j)
+print('两次以上热度词', s3)
