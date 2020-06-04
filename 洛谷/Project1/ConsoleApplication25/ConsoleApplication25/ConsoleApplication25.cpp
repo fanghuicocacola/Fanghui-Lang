7bec8a6cead3926b2bfe8da882096ea9f3d5a3e5
辉 房 <fanghuicocacola@outlook.com>
2020-06-04 19:50:58 +0800

    daka

---
+++ 洛谷/Project1/ConsoleApplication25/ConsoleApplication25/ConsoleApplication25.cpp
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+int y[10];
+int jc(int n)
+{
+	if (n == 1)
+		return 1;
+	else
+		return n * jc(n - 1);
+}
+void find(int x)
+{
+	memset(y, 0, sizeof(y));
+	while (x > 9) {
+		y[x % 10] += 1;
+		x /= 10;
+	}
+	y[x % 10] += 1;
+}
+int main()
+{
+	int t, n, a, c, num;
+	num = 0;
+	scanf_s("%d", &t);
+	for (int i = 0; i < t; i++) {
+		scanf_s("%d %d", &n, &a);
+		c = jc(n);
+		find(c);
+		printf("%d", y[a]);
+	}
+}
+
+
+
+
