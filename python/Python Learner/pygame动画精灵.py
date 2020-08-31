01da0d6954f49e20c35b7b52617bd6e3a6b3ad60
辉 房 <fanghuicocacola@outlook.com>
2020-08-31 09:44:49 +0800

    添加碰撞功能

--- python/Python Learner/pygame动画精灵.py
+++ python/Python Learner/pygame动画精灵.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 from pygame.locals import *
+import math
 from random import *
 
 
@@ -9,7 +10,7 @@ class Ball(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(image).convert_alpha()
         self.rect = self.image.get_rect()
-        position = self.rect.left, self.rect.top
+        self.rect.left, self.rect.top = position
         self.speed = speed
         self.width, self.height = bg_size[0], bg_size[1]
 
@@ -25,21 +26,38 @@ class Ball(pygame.sprite.Sprite):
             self.rect.bottom = 0
 
 
+def collide_check(item, target):
+    col_balls = []
+    for each in target:
+        distance = math.sqrt(\
+            math.pow((item.rect.center[0] - each.rect.center[0]), 2) + \
+            math.pow((item.rect.center[1] - each.rect.center[1]), 2))
+        if distance <= (item.rect.width + each.rect.width) / 2:
+            col_balls.append(each)
+
+    return col_balls
+
+
 def main():
     pygame.init()
-    plane_image = 'ball.png'
+    ball_image = 'ball.png'
     bg_image = 'background.png'
-    bg_size = width, height = 487, 704
+    bg_size = width, height = 1313, 738
     screen = pygame.display.set_mode(bg_size)
     pygame.display.set_caption("demo")
 
     running = True
     background = pygame.image.load(bg_image).convert_alpha()
     balls = []
-    for i in range(1):
-        position = randint(0, width-400)
-        speed = [randint(-10, 10),randint(-10, 10)]
-        ball = Ball(plane_image, position, speed, bg_size)
+    Ball_Num = 5
+
+    for i in range(Ball_Num):
+        position = randint(0, width-400), randint(0, height-400)
+        print(position)
+        speed = [randint(-10, 10), randint(-10, 10)]
+        ball = Ball(ball_image, position, speed, bg_size)
+        while collide_check(ball, balls):
+            ball.rect.left, ball.rect.top = randint(0, width-400), randint(0, height-400)
         balls.append(ball)
     clock = pygame.time.Clock()
 
@@ -54,8 +72,15 @@ def main():
             each.move()
             screen.blit(each.image, each.rect)
 
+        for i in range(Ball_Num):
+            item = balls.pop(i)
+            if collide_check(item, balls):
+                item.speed[0] = -item.speed[0]
+                item.speed[1] = -item.speed[1]
+            balls.insert(i, item)
+
         pygame.display.flip()
-        clock.tick(36)
+        clock.tick(30)
 
 
 if __name__ == "__main__":
