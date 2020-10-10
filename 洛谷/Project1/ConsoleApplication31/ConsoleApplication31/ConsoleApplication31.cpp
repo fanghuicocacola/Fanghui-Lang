4640199a856bd87724f32d3de588c6de4a84301f
辉 房 <fanghuicocacola@outlook.com>
2020-10-10 19:44:30 +0800

    daka

---
+++ 洛谷/Project1/ConsoleApplication31/ConsoleApplication31/ConsoleApplication31.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int total(int i)
+{
+    if (i == 1)
+        return 1;
+    return total(i - 1) * i;
+}
+int main()
+{
+    int n;
+    long long int ans;
+    scanf_s("%d", &n);
+    printf("%d", total(n));
+}
+
+
