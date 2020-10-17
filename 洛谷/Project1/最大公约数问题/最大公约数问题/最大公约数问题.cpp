3e9cbfd48e7cbe34d5a16a5b3939aa123e3df74d
辉 房 <fanghuicocacola@outlook.com>
2020-10-17 10:48:14 +0800

    daka

---
+++ 洛谷/Project1/最大公约数问题/最大公约数问题/最大公约数问题.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+long long a, b, c;
+
+long long maxgyc(long long x, long long y) 
+{
+	// 辗转相除法
+	if (x % y == 0) 
+		return y;
+	return maxgyc(y, x % y);//重点
+}
+
+long long mingbc(long long x, long long y)
+{	//最小公倍数:相乘除以最大公约数
+	return x * y / maxgyc(x, y);
+}
+
+int main()
+{
+	scanf("%lld%lld%lld", &a, &b, &c);
+	long long ans = 0;
+	ans = mingbc(a, mingbc(b, c));
+	cout << ans << endl;
+	return 0;
+}
+
