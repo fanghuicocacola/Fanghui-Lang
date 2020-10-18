ec8a7465ef0b4338146c38079488924af8b4066e
辉 房 <fanghuicocacola@outlook.com>
2020-10-18 12:07:53 +0800

    daka

---
+++ 洛谷/Project1/数正方形/数正方形/数正方形.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<string.h>
+using namespace std;
+int main() {
+	int N, M,n;
+	cin >> N >> M;
+	int cnt = 0;
+	n = min(N, M);
+	for (int i = 0; i < n; i++)
+	{
+		cnt += (N-i)*(M-i);
+	}
+	cout << cnt;
+	return 0;
+}
