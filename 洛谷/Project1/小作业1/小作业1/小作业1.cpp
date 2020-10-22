9638cd533c0190e50b069b17e9bfccded456e057
辉 房 <fanghuicocacola@outlook.com>
2020-10-22 23:34:01 +0800

    dakai

---
+++ 洛谷/Project1/小作业1/小作业1/小作业1.cpp
@@ -0,0 +1,20 @@
+﻿#include<stdio.h>
+
+int main() {
+    int a[4], temp;
+    for (int i = 0; i < 4; i++)
+        scanf("%d", &a[i]);
+    for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < 3 - j; i++) {
+            if (a[i] > a[i + 1])
+            {
+                temp = a[i]; a[i] = a[i + 1]; a[i + 1] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < 4; i++)
+        printf("%d", a[i]);
+    return 0;
+}
+
+
