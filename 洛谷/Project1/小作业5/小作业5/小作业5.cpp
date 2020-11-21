3ace23904516266b791cf2a6eee36628bb337cdc
辉 房 <fanghuicocacola@outlook.com>
2020-11-21 15:49:24 +0800

    daka

---
+++ 洛谷/Project1/小作业5/小作业5/小作业5.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int a;
+	int b, lengh = 1, n;
+	for (int i = 2; i < 10000; i++)
+	{
+		a = i * i;
+		b = i;
+		while (b > 0)
+		{
+			b /= 10;
+			lengh *= 10;
+		}
+		n = a % lengh;
+		if (i == n) 
+		{
+			printf("%d   ", lengh);
+			cout << i << endl;
+		}
+		lengh = 1;
+	}
+}
+
+
---
+++ 洛谷/Project1/小作业8/小作业8/小作业8.cpp
@@ -0,0 +1,19 @@
+﻿#include<stdio.h>
+int main()
+{
+	int n;
+	float t, r = 0, fm = 2, fz = 1;
+	scanf_s("%d", &n);
+	while (n--)
+	{
+		r += fm / fz;
+		t = fm;
+		fm += fz;
+		fz = t;
+	}
+	printf("%.3f", r);
+	return 0;
+}
+
+
+
