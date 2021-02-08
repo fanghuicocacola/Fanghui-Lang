bca35971797a7a6bd67ba2fd3b0db7ccd53ed1cf
fanghuicocacola <fanghuicocacola@outlook.com>
2021-02-08 20:22:14 +0800

    daka

--- 洛谷/Project1/P1878舞蹈课/P1878舞蹈课/P1878舞蹈课.cpp
+++ 洛谷/Project1/P1878舞蹈课/P1878舞蹈课/P1878舞蹈课.cpp
@@ -3,61 +3,65 @@
 #include<queue>
 #include<cmath>
 using namespace std;
-#define inf 2000000000
+#define inf 999999999
 int ans[200005][2], sum = 0;
-struct node//链表 
+struct node
 {
-	int l, r, cha, xu;
+	int l, r, cha, num;
 	bool judge;
 }a[200005];
-
 bool operator<(node rr, node ll)//优先队列有限法则 
 {
 	int lll = abs(ll.cha), rrr = abs(rr.cha);
 	if (rrr != lll)
 		return lll < rrr;
 	else
-		return ll.xu < rr.xu;
+		return ll.num < rr.num;
 }
 
-priority_queue <node> que;
+priority_queue <node> q;
 
-int main()
-{
+int main()//输入人数，判断性别，judge=0,男，=0，女，=1
+{//记录技术值（换成now，next的差值，存入a[i].cha,a[i].num存自身位置，
 	int n, now, next, t = 0;
 	char x;
 	scanf_s("%d", &n);
 	for (int i = 1; i <= n; i++)
 	{
-		cin >> x;
-		if (x == 'B') a[i].judge = 0;
-		else a[i].judge = 1;
-	} //以上性别判断 
+		cin >>x;
+		if (x == 'B') 
+			a[i].judge = 0;
+		else 
+			a[i].judge = 1;
+	} //性别存入 
 	scanf_s("%d", &now);
 	for (int i = 1; i < n; i++)
 	{
 		scanf_s("%d", &next);
-		a[i].xu = i;	 		//自身是第几个 
-		a[i].cha = next - now;//预处理技术值差 
-		a[i].l = i - 1; a[i].r = i + 1;//预处理左右 
-		que.push(a[i]);
+		a[i].num = i;	 		 
+		a[i].cha = next - now; 
+		a[i].l = i - 1; a[i].r = i + 1;
+		q.push(a[i]);
 		now = next;
-	}
-	a[n].r = n + 1; a[n].xu = n; a[n].cha = inf;
-	while (!que.empty())//开始模拟过程 
+	}//技术值存入，并计算差值
+	a[n].r = n + 1; a[n].num = n; a[n].cha = inf;//因为是最后一个人了，右边实际上没人，差值inf
+	while (!q.empty())
 	{
-		int xx = que.top().xu, cmp = que.top().cha;
-		que.pop();
+		int xx = q.top().num, cmp = q.top().cha;
+		q.pop();//取出栈顶元素并删除
 		int yy = a[xx].r;
-		if (a[xx].cha == cmp/*判断是否是更新过的*/ && a[xx].judge != a[yy].judge/*一男一女*/
-			&& xx != 0 && yy != n + 1/*特判链表首尾 */)
+		if (a[xx].cha == cmp 
+			&& a[xx].judge != a[yy].judge//本身和他右边的是否为异性
+			&& xx != 0 //不是最左边的
+			&& yy != n + 1)//不是最左边的
 		{
-			ans[++t][0] = xx; ans[t][1] = yy;
-			a[a[xx].l].cha += a[xx].cha + a[yy].cha;//更新差值 
-			a[a[yy].r].l = a[xx].l; a[a[xx].l].r = a[yy].r;//断链重连 
-			if (a[xx].l > 0 && a[yy].r <= n)//特判链表首尾 
-				que.push(a[a[xx].l]);//入队列 
-			a[xx].l = a[xx].r = 0; a[yy].l = a[yy].r = n + 1;
+			ans[++t][0] = xx; ans[t][1] = yy;//++t先进行自增，再计算表达式，t表示一个数，1是它（0）的右值
+			a[a[xx].l].cha += a[xx].cha + a[yy].cha;//差值更新
+			a[a[yy].r].l = a[xx].l; // 例a b c d删去b c 
+			a[a[xx].l].r = a[yy].r;
+			if (a[xx].l > 0 && a[yy].r <= n)
+				q.push(a[a[xx].l]);
+			a[xx].l = a[xx].r = a[yy].l = a[yy].r = n + 1;//比较过的先筛选掉
 		}
 	}
 	printf("%d\n", t);
