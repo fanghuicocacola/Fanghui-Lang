4b033ac1a3502a43997fd868691707d5ede12ead
fanghuicocacola <fanghuicocacola@outlook.com>
2021-02-13 18:26:36 +0800

    daka

---
+++ 洛谷/Project1/P1090合并果子/P1090合并果子/P1090合并果子.cpp
@@ -0,0 +1,27 @@
+﻿#include<stdio.h>
+#include<algorithm>
+using namespace std;
+int a[100005];
+int main()
+{
+	int n;
+	scanf_s("%d", &n);
+	for (int i = 0; i < n; i++)
+		scanf_s("%d", &a[i]);
+	sort(a, a + n);
+	int ans = 0;
+	for (int i = 0; i < n-1 ; i++)
+	{
+		ans += (a[i] + a[i + 1]);
+		a[i + 1] += a[i];
+		for (int j = i + 1; j < n-1; j++)
+		{
+			if (a[j] > a[j + 1])
+				swap(a[j], a[j + 1]);
+			else
+				break;
+		}
+	}
+	printf("%d\n", ans);
+	return 0;
+}
