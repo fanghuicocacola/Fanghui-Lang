d8455ec3b45937a7c1e22bb582af086bdd93e0e8
fanghuicocacola <fanghuicocacola@outlook.com>
2021-02-16 13:14:48 +0800

    daka

---
+++ 洛谷/Project1/P5709/P5709/P5709.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>//万能头文件
+using namespace std;
+int m, t, s;
+int main()
+{
+	cin >> m >> t >> s;
+	if (t == 0)
+	{
+		cout << 0;
+		return 0;
+	}
+	if (s % t == 0)
+		cout << max(m - s / t, 0);
+	else
+		cout << max(m - s / t - 1, 0);
+	return 0;
+}
