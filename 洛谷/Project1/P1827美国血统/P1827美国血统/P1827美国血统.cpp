939705818d041cf76e9c961ff20920e13ea69b1e
fanghuicocacola <fanghuicocacola@outlook.com>
2021-02-17 15:21:05 +0800

    daka

---
+++ 洛谷/Project1/P1827美国血统/P1827美国血统/P1827美国血统.cpp
@@ -0,0 +1,22 @@
+﻿#include <string>
+#include <iostream>
+using namespace std;
+
+void DFS(string aa, string bb)
+{
+	if (!(int)bb.size())
+		return;
+	int pos = aa.find(bb[0]);
+	DFS(aa.substr(0, pos), bb.substr(1, pos)); 
+	DFS(aa.substr(pos + 1), bb.substr(pos + 1)); 
+	printf("%c", bb[0]); 
+	return; 
+
+}
+int main()
+{
+	string a, b;
+	cin >> a >> b;
+	DFS(a, b);
+	return 0;
+}
