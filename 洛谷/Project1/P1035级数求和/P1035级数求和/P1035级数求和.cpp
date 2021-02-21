94e96e3d28607f5977d219c3a076aa6380ebccd7
fanghuicocacola <fanghuicocacola@outlook.com>
2021-02-21 15:12:19 +0800

    daka

---
+++ 洛谷/Project1/P1035级数求和/P1035级数求和/P1035级数求和.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+	int n = 0,k;
+	double Sn = 0.00000;
+	cin >> k;
+	while (Sn <= k)
+	{
+		n++;
+		Sn += 1.00000 / n;
+	}
+	cout << n;
+}
