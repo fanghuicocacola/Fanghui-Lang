a72295e2d8fefa51071a52c058e01217728f1a80
fanghuicocacola <fanghuicocacola@outlook.com>
2021-03-12 19:38:55 +0800

    daka

---
+++ 洛谷/Project1/P5706再分肥宅水/P5706再分肥宅水/P5706再分肥宅水.cpp
@@ -0,0 +1,19 @@
+﻿// P5706再分肥宅水.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+using namespace std;
+#include <stdio.h>
+int main()
+{
+    int a, m;
+    float sum, n;
+    cin >> sum >> a;
+    n = sum / a;
+    m = 2 * a;
+    printf_s("%.3f\n",n);
+    printf_s("%d", m);
+}
+
+
+
