cc46d7655e9d4dc9965ca06f2b218b7ae85d4366
fanghuicocacola <fanghuicocacola@outlook.com>
2021-03-12 19:37:45 +0800

    daka

--- JAVA/.vscode/java1.java
+++ JAVA/.vscode/java1.java
@@ -1,6 +1,6 @@
 public class java1{
     public static void main(String[] args){ 
-        System.out.println("HelloWord!嗷嗷嗷");
-        System.out.println("HelloWord");
+        System.out.println("Hello");
+        System.out.println("World");
     }
 }
