0f47dda44effbdff2b21073749f94b9e34651f0f
fanghuicocacola <fanghuicocacola@outlook.com>
2021-05-20 10:35:50 +0800

    daka

---
+++ JAVA/.vscode/Tokenizer.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Tokenizer {
+    public static void main(String[] args) {
+        String s = "We are students, hello world.Yes";
+        StringTokenizer fenxi = new StringTokenizer(s,", .");
+        System.out.println(fenxi.countTokens());
+        while(fenxi.hasMoreTokens()){
+            String a = fenxi.nextToken();
+            System.out.println("切割"+a+"还剩"+fenxi.countTokens());
+        }
+    }
+}
