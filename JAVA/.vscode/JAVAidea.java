077ee92af4a5d72b8cbfb99dea69e68568da6477
Fanghui <fanghuicocacola@outlook.com>
2024-09-09 11:57:29 +0800

    对仓库进行精简

--- JAVA/.vscode/JAVAidea.java
+++
@@ -1,5 +0,0 @@
-public class JAVAidea{
-    public static void main(String[] args) {
-        System.out.println("111");
-    }
-}
--- JAVA/.vscode/java1.java
+++
@@ -1,6 +0,0 @@
-public class java1{
-    public static void main(String[] args){ 
-        System.out.println("Hello");
-        System.out.println("World");
-    }
-}
